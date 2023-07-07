60496a59222175e8137997717404f452e7643659
muhammadnazreen <nazreenmubin00@gmail.com>
2023-07-07 12:25:07 +0800

    updated database

--- database/loginsys_db.sql
+++ database/loginsys_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 15, 2023 at 03:44 PM
+-- Generation Time: Jul 07, 2023 at 06:23 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -28,19 +28,23 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int(30) NOT NULL,
   `user_name` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL
+  `email` varchar(255) NOT NULL,
+  `secret_question` varchar(255) NOT NULL,
+  `secret_answer` varchar(255) NOT NULL,
+  `encryption_key` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `user_name`, `password`, `name`, `email`) VALUES
-(1, 'admin', '123', 'administrator', `admin@gmail.com`);
+INSERT INTO `users` (`id`, `user_name`, `password`, `name`, `email`, `secret_question`, `secret_answer`, `encryption_key`) VALUES
+(10, 'admin', '0e7517141fb53f21ee439b355b5a1d0a', 'admin', '', 'What is your pet&#039;s name?', 'QE8snFu5r8petRiRnWf/sTlrWXlvQUVJNGVmYnZNclRjUHl2N2c9PQ==', '107475b036cc610aae5610e6fae9c7ea'),
+(11, 'testt', '029db36a20d7a35c4cc93f6f9457f74c', 'testt', '', 'What is your pet&#039;s name?', '12vNyFU=', 'testt');
 
 --
 -- Indexes for dumped tables
@@ -60,7 +64,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
