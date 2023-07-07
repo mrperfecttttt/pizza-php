dec86391950bdc4d3b8ec8a1fa5dedfd8931ce6b
muhammadnazreen <nazreenmubin00@gmail.com>
2023-07-07 18:44:15 +0800

    Update cart

--- cart.php
+++ cart.php
@@ -55,94 +55,93 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 
                 <input type="hidden" name="csrf_token" value="<?php echo $csrfToken; ?>">
                 <?php
-                if (isset($item1Price)) {
-                    echo '<div class="cart-item">
-                    <h2 class="item-name">Deluxe Cheese</h2>
-                    <input type="hidden" name="quantity1" value="<?php echo $quantity1 ?>">
-                    <div class="item-details">
-                        <div class="quantity-container">
-                            <span class="quantity"><?php echo $quantity1 ?></span>
+
+
+                if (isset($quantity1) || isset($quantity2) || isset($quantity3)) {
+
+                    echo '<table class="styled-table" id="tableOne">
+                    <thead>
+                        <tr>
+                            <th>Items</th>
+                            <th>Quantity</th>
+                            <th>Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>';
+                    if ($quantity1 > 0) {
+                        echo '<tr>
+                        <td>Deluxe Cheese</td>
+                        <td>' . $quantity1 . '</td>
+                        <td>RM' . (number_format($item1Price, 2)) . '</td></tr>';
+                    }
+                    if ($quantity2 > 0) {
+                        echo '<tr>
+                        <td>Four of a Kind Cheese</td>
+                        <td>' . $quantity2 . '</td>
+                        <td>RM' . (number_format($item2Price, 2)) . '</td></tr>';
+                    }
+                    if ($quantity3 > 0) {
+                        echo '<tr>
+                        <td>Chicken Pepperoni</td>
+                        <td>' . $quantity3 . '</td>
+                        <td>RM' . (number_format($item3Price, 2)) . '</td></tr>';
+                    }
+
+
+
+                    echo '<tfoot>
+                            <td colspan="2">Total Price:</td>
+                            <td id="totalPrice">RM' . (number_format($totalPrice, 2)) . '</td>
+                        </tfoot>
+                    </tbody>
+                    </table>
+
+                    <div class="checkout-form">
+
+                        <h2 class="form-heading">Delivery Details</h2>
+                        <div class="form-row">
+                            <label for="location">Location:</label>
+                            <input type="text" id="location" name="location" placeholder="Enter Address Line 1" required>
                         </div>
-                        <span class="item-price dark-grey round">RM' . (number_format($item1Price, 2)) . '</span>
-                    </div>
-                </div>';
-                }
-                if (isset($item2Price)) {
-                    echo '<div class="cart-item">
-                    <h2 class="item-name">Four of a Kind Cheese</h2>
-                    <input type="hidden" name="quantity2" value="<?php echo $quantity2 ?>">
-                    <div class="item-details">
-                        <div class="quantity-container">
-                            <span class="quantity"><?php echo $quantity2 ?></span>
+                        <div class="form-row">
+                            <label for="address-line2"></label>
+                            <input type="text" id="location" name="location2" placeholder="Enter Address Line 2" required>
                         </div>
-                        <span class="item-price dark-grey round">RM' . (number_format($item2Price, 2)) . '</span>
-                    </div>
-                </div>';
-                }
-                if (isset($item3Price)) {
-                    echo '<div class="cart-item">
-                    <h2 class="item-name">Chicken Pepperoni <span class="tag red round">Hot!</span></h2>
-                    <input type="hidden" name="quantity3" value="<?php echo $quantity3 ?>">
-                    <div class="item-details">
-                        <div class="quantity-container">
-                            <span class="quantity"><?php echo $quantity3 ?></span>
+
+
+
+                        <div class="form-row">
+                            <label for="delivery-date">Delivery Date:</label>
+                            <input type="date" id="delivery-date" name="delivery-date" required>
                         </div>
-                        <span class="item-price dark-grey round">RM' . (number_format($item1Price, 2)) . '</span>
+
+                        <div class="form-row">
+                            <label for="delivery-time">Delivery Time:</label>
+                            <input type="time" id="delivery-time" name="delivery-time" required>
+                        </div>
+
+
+                        <h2 class="form-heading">Credit Card Information</h2>
+                        <div class="form-row">
+                            <label for="card-num">Card Number:</label>
+                            <input type="text" id="card-num" name="card-num" required pattern="[0-9]{16}" title="Please enter a 16-digit card number">
+                        </div>
+
+                        <div class="form-row">
+                            <label for="card-expiry-date">Expiry Date:</label>
+                            <input type="text" id="card-expiry-date" name="card-expiry-date" required pattern="^(0[1-9]|1[0-2])\/?([0-9]{2}|[0-9]{2})$" title="Please enter a valid expiry date (MM/YY)">
+                        </div>
+
+                        <div class="form-row">
+                            <label for="card-cvv">CVV:</label>
+                            <input type="text" id="card-cvv" name="card-cvv" required pattern="[0-9]{3}" title="Please enter a 3-digit CVV">
+                        </div>
+
+                        <div class="form-row">
+                            <input type="submit" value="Proceed to Checkout" class="checkout-button">
+                        </div>
                     </div>
-                </div>';
-                }
-                if (isset($item1Price) || isset($item2Price) || isset($item3Price)) {
-
-                    echo '<div class="total-container right tag">
-                    <h2 class="total-price">Total Price: RM' . (number_format($totalPrice, 2)) . '</h2>
-                    <input type="hidden" name="total_price" value="<?php echo $totalPrice ?>">
-                </div>
-                <div class="checkout-form">
-
-                    <h2 class="form-heading">Delivery Details</h2>
-                    <div class="form-row">
-                        <label for="location">Location:</label>
-                        <input type="text" id="location" name="location" placeholder="Enter Address Line 1" required>
-                    </div>
-                    <div class="form-row">
-                        <label for="address-line2"></label>
-                        <input type="text" id="location" name="location2" placeholder="Enter Address Line 2" required>
-                    </div>
-
-
-
-                    <div class="form-row">
-                        <label for="delivery-date">Delivery Date:</label>
-                        <input type="date" id="delivery-date" name="delivery-date" required>
-                    </div>
-
-                    <div class="form-row">
-                        <label for="delivery-time">Delivery Time:</label>
-                        <input type="time" id="delivery-time" name="delivery-time" required>
-                    </div>
-
-
-                    <h2 class="form-heading">Credit Card Information</h2>
-                    <div class="form-row">
-                        <label for="card-num">Card Number:</label>
-                        <input type="text" id="card-num" name="card-num" required pattern="[0-9]{16}" title="Please enter a 16-digit card number">
-                    </div>
-
-                    <div class="form-row">
-                        <label for="card-expiry-date">Expiry Date:</label>
-                        <input type="text" id="card-expiry-date" name="card-expiry-date" required pattern="^(0[1-9]|1[0-2])\/?([0-9]{2}|[0-9]{2})$" title="Please enter a valid expiry date (MM/YY)">
-                    </div>
-
-                    <div class="form-row">
-                        <label for="card-cvv">CVV:</label>
-                        <input type="text" id="card-cvv" name="card-cvv" required pattern="[0-9]{3}" title="Please enter a 3-digit CVV">
-                    </div>
-
-                    <div class="form-row">
-                        <input type="submit" value="Proceed to Checkout" class="checkout-button">
-                    </div>
-                </div>
-                ';
+                    ';
                 } else {
                     echo '<div class="cart-item">
                     <h2 class="item-name">No item listed in your cart</h2>
